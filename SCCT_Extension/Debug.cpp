15bc0bcd7c7ec0b6244bdc0cc1e8658d7d8bb020
AllyPal <142358726+AllyPal@users.noreply.github.com>
2024-10-06 18:52:44 +0100

    add debug command handlers

--- SCCT_Extension/Debug.cpp
+++ SCCT_Extension/Debug.cpp
@@ -2,6 +2,8 @@
 #include "Debug.h"
 #include "MemoryWriter.h"
 
-void Debug::Initialize()
-{
+void Debug::Initialize() {
+}
+
+void Debug::CommandHandlers(std::map<std::wstring, CommandHandler>* commandHandlers) {
 }
--- SCCT_Extension/Debug.h
+++ SCCT_Extension/Debug.h
@@ -3,4 +3,5 @@ class Debug
 {
 public:
     static void Initialize();
+    static void CommandHandlers(std::map<std::wstring, CommandHandler>* commandHandlers);
 };
